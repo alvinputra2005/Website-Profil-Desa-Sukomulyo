af7df6a9e54f68388030ca61883e12e4e9fe74cd
qurrotulaini0325 <qurrotulaini0325@gmail.com>
2026-08-02 20:29:07 +0700

    feat: enhance population management UI with improved action button layout and responsiveness

--- resources/views/admin/population/residents/index.blade.php
+++ resources/views/admin/population/residents/index.blade.php
@@ -5,15 +5,15 @@
 <div class="box box-info">
     <div class="box-header with-border">
         <h3 class="box-title"><i class="fa fa-users"></i> Daftar Penduduk</h3>
-        <div class="box-tools">
+        <div class="box-tools population-header-actions">
+            <a href="{{ route('admin.population.residents.create') }}" class="btn btn-social btn-info btn-sm"><i class="fa fa-plus"></i> Tambah Penduduk</a>
+            <button type="button" class="btn btn-social btn-success btn-sm" data-toggle="modal" data-target="#import-residents"><i class="fa fa-file-excel-o"></i> Impor Excel</button>
             <form id="bulk-delete-residents" method="post" action="{{ route('admin.population.residents.bulk-destroy') }}" class="inline-form" data-confirm="Arsipkan data penduduk yang dipilih?" data-confirm-tone="danger" data-confirm-title="Arsipkan Penduduk Terpilih" data-confirm-button="Ya, arsipkan">
                 @csrf
                 @method('delete')
                 <button class="btn btn-danger btn-sm" type="submit" data-bulk-button disabled><i class="fa fa-archive"></i> Arsipkan Terpilih</button>
             </form>
-            <button type="button" class="btn btn-social btn-success btn-sm" data-toggle="modal" data-target="#import-residents"><i class="fa fa-file-excel-o"></i> Impor Excel</button>
             <a href="{{ route('admin.population.residents.archive') }}" class="btn btn-default btn-sm"><i class="fa fa-archive"></i> Arsip</a>
-            <a href="{{ route('admin.population.residents.create') }}" class="btn btn-social btn-info btn-sm"><i class="fa fa-plus"></i> Tambah Penduduk</a>
         </div>
     </div>
     <div class="box-body">
